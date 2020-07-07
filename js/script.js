ad906be222062da9c9ac98f4839f5ee038c06a36
Tommaso Venza <tommasovenza@gmail.com>
2020-07-07 13:21:59 +0200

    upload files iniziali

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Utilizzando le API Todos di Boolean creiamo un’interfaccia in cui possiamo leggere, creare e rimuovere degli elementi da una todo-list
+// Endpoints:
+// Read: http://157.230.17.132:{porta}/todos/ Method:GET
+// Create: http://157.230.17.132:{porta}/todos/ Method: POST
+// Che si aspetta il testo da salvare (nome proprietà ‘text’)
+// Delete: http://157.230.17.132:{porta}/todos/{id} Method: DELETE
+// Dove id è l’id della risorsa da eliminare
+// Ognuno di voi ha un porta assegnata da includere nell’url, questo è il documento con le porte.
+// https://docs.google.com/spreadsheets/d/1_Yo6HzbTmZ8NXJtkR4VJ4CetEyxT5ShmH-SWjGzXsaA/edit#gid=0
+// Accanto al vostro nome trovate la porta che dovrete utilizzare.
+// Buon lavoro!
+
+// mia porta 3034
+
